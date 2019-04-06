84a1b6f24a09f7db59de5d518a31ab74e5791c20
Kazuyuki SUZUKI <kechol@users.noreply.github.com>
2019-04-06 20:51:48 +0900

    Install css-loader and ts-loader

--- src/inject/index.ts
+++ src/inject/index.ts
@@ -1,5 +1,5 @@
-chrome.extension.sendMessage({}, function (response) {
-  var readyStateCheckInterval = setInterval(function () {
+chrome.runtime.sendMessage({}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === "complete") {
       clearInterval(readyStateCheckInterval);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/inject/index.js',
+  entry: './src/inject/index.ts',
   output: {
     filename: 'inject.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader'
+      }],
+    }, {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }],
+  },
 };
